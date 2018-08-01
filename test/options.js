d888152e9bc67008d8b7be41b351d606c6f2c7ea
Alex Fernández <alexfernandeznpm@gmail.com>
2018-08-01 17:20:05 +0000

    Added test for trailing /. Also for missing url.

--- test/options.js
+++ test/options.js
@@ -4,16 +4,17 @@ require('should');
 
 const config = require('../lib/config.js');
 
-const FROM = '2018-07-09T09:00Z';
-const TO = '2018-07-09T09:30Z';
-const QUERY =
+const dateFrom = '2018-07-09T09:00Z';
+const dateTo = '2018-07-09T09:30Z';
+const query =
   'select eventdate, sessionid, level from demo.ecommerce.data';
-const QUERY_ID = '1234566789';
-const API_KEY =
+const queryId = '1234566789';
+const apiKey =
   'fakekey';
-const API_SECRET =
+const apiSecret =
   'fakesecret';
-const TOKEN = 'faketoken';
+const token = 'faketoken';
+const url = 'http://test.test/test';
 
 describe('Config validation', () => {
 
@@ -24,20 +25,30 @@ describe('Config validation', () => {
 
   it('parses valid config', () => {
     const opc = config.create({
-      apiKey: API_KEY,
-      apiSecret: API_SECRET,
+      url,
+      apiKey,
+      apiSecret,
     }).parseQuery({
-      dateFrom: FROM,
-      dateTo: TO,
-      query: QUERY,
+      dateFrom,
+      dateTo,
+      query,
     });
     config.validate(opc).should.equal(true);
   });
 
-  it('parses invalid config without auth', () => {
+  it('parses invalid config without url', () => {
     const opc = config.create({}).parseQuery({
-      dateFrom: FROM,
-      dateTo: TO,
+      dateFrom,
+      dateTo,
+      queryId: '123456',
+    });
+    config.validate(opc).should.equal(false);
+  });
+
+  it('parses invalid config without auth', () => {
+    const opc = config.create({url}).parseQuery({
+      dateFrom,
+      dateTo,
       queryId: '123456',
     });
     config.validate(opc).should.equal(false);
@@ -45,59 +56,83 @@ describe('Config validation', () => {
 
   it('parses invalid config without query or queryId', () => {
     const opc = config.create({
-      apiKey: API_KEY,
-      apiSecret: API_SECRET,
+      apiKey,
+      apiSecret,
     }).parseQuery({
-      dateFrom: FROM,
-      dateTo: TO,
+      dateFrom,
+      dateTo,
     });
     config.validate(opc).should.equal(false);
   });
 
   it('parses valid config with query param and apikey', () => {
     const opc = config.create({
-      apiKey: API_KEY,
-      apiSecret: API_SECRET,
+      url,
+      apiKey,
+      apiSecret,
     }).parseQuery({
-      dateFrom: FROM,
-      dateTo: TO,
-      query: QUERY,
+      dateFrom,
+      dateTo,
+      query,
     });
     config.validate(opc).should.equal(true);
   });
 
   it('parses valid config with query param and token', () => {
     const opc = config.create({
-      token: TOKEN,
+      url,
+      token,
     }).parseQuery({
-      dateFrom: FROM,
-      dateTo: TO,
-      query: QUERY,
+      dateFrom,
+      dateTo,
+      query,
     });
     config.validate(opc).should.equal(true);
   });
 
   it('parses valid config with queryId param and apikey', () => {
     const opc = config.create({
-      apiKey: API_KEY,
-      apiSecret: API_SECRET,
+      url,
+      apiKey,
+      apiSecret,
     }).parseQuery({
-      dateFrom: FROM,
-      dateTo: TO,
-      queryId: QUERY_ID,
+      dateFrom,
+      dateTo,
+      queryId,
     });
     config.validate(opc).should.equal(true);
   });
 
   it('parses valid config with queryId param and token', () => {
     const opc = config.create({
-      token: TOKEN,
+      url,
+      token,
     }).parseQuery({
-      dateFrom: FROM,
-      dateTo: TO,
-      queryId: QUERY_ID,
+      dateFrom,
+      dateTo,
+      queryId,
     });
     config.validate(opc).should.equal(true);
+  });
+
+  it('parses URL that ends with and without /', () => {
+    const opc1 = config.create({
+      url: 'http://test.test/test/',
+      token,
+    }).parseQuery({
+      dateFrom,
+      dateTo,
+      queryId,
+    });
+    const opc2 = config.create({
+      url,
+      token,
+    }).parseQuery({
+      dateFrom,
+      dateTo,
+      queryId,
+    });
+    opc1.url.should.equal(opc2.url)
   });
 });
 
